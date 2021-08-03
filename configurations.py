837239f0a034251c2475a7611da00e68cddeccdb
prince sudhanshu <sudhanshuprincestm@gmail.com>
2021-08-04 02:37:59 +0530

    initial commit

---
+++ configurations.py
@@ -0,0 +1,8 @@
+config = {
+    "apiKey": "AIzaSyDfVQgxa1Glof33jc2aaHwuV9B18YoUng4",
+    "authDomain": "subjective-answer-evalua-c7ffb.firebaseapp.com",
+    "databaseURL": "https://subjective-answer-evalua-c7ffb-default-rtdb.firebaseio.com",
+    "projectId": "subjective-answer-evalua-c7ffb",
+    "storageBucket": "subjective-answer-evalua-c7ffb.appspot.com",
+    "messagingSenderId": "775806534603"
+}
